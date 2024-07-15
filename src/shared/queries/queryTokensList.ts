84c3239c849bd00233a6cc7b69b93f4170d79ee4
Ilia Krasiuchenko <thesixteensaltines@gmail.com>
2024-07-15 14:31:01 +0700

    refact: reuse getBackendUrl

--- src/shared/queries/queryTokensList.ts
+++ src/shared/queries/queryTokensList.ts
@@ -1,8 +1,9 @@
+import { getBackendUrl } from "./const";
 import { TokenRaw } from "./types";
 
 export async function queryTokensList(chainId: number): Promise<TokenRaw[]> {
   try {
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/external/tokensList/${chainId}`);
+    const response = await fetch(`${getBackendUrl()}/external/tokensList/${chainId}`);
     const tokenList = await response.json();
 
     return tokenList;
