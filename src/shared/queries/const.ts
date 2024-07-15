bd1ddf0707d09293e2bbe11df1eeddf503c722b3
Ilia Krasiuchenko <thesixteensaltines@gmail.com>
2024-07-15 14:28:32 +0700

    fix backend url access

--- src/shared/queries/const.ts
+++ src/shared/queries/const.ts
@@ -1,1 +1,11 @@
 export const QUERY_TOKENS_METADATA_LIMIT = 120;
+
+export const getBackendUrl = () => {
+  const url = import.meta.env.VITE_BACKEND_URL;
+
+  if (!url) {
+    throw new Error("Backend URL is not set");
+  }
+
+  return url;
+};
--- src/shared/queries/queryTokenMetadata.ts
+++ src/shared/queries/queryTokenMetadata.ts
@@ -1,8 +1,9 @@
+import { getBackendUrl } from "./const";
 import { TokenWithMetadata } from "./types";
 
 export async function queryTokensMetadata(tokens: string[], chainId: number): Promise<TokenWithMetadata[]> {
   try {
-    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/moralis/queryTokensMetadata`, {
+    const response = await fetch(`${getBackendUrl()}/moralis/queryTokensMetadata`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
